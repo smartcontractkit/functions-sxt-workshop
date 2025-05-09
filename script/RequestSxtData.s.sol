6af27bd0ad3744410c76e5f8889f99f40d469efb
Thomas Hodges <thomas@smartcontract.com>
2025-05-08 19:50:11 -0500

    Update SQL query

--- script/RequestSxtData.s.sol
+++ script/RequestSxtData.s.sol
@@ -15,7 +15,7 @@ import {SxtNumericQuery} from "../src/SxtNumericQuery.sol"; // Import the correc
 contract RequestSxtData is Script {
     // The SXT SQL query to execute
     string public constant SXT_QUERY =
-        "SELECT AVG(BITCOIN.STATS.AVG_FEERATE) FROM BITCOIN.STATS WHERE TIME_STAMP >= CURRENT_DATE - INTERVAL '3 days' AND TIME_STAMP < CURRENT_DATE + INTERVAL '1 day';";
+        "SELECT AVG(BITCOIN.STATS.AVG_FEERATE) AS AVG_FEE FROM BITCOIN.STATS WHERE TIME_STAMP >= CURRENT_DATE - INTERVAL 14 days AND TIME_STAMP < CURRENT_DATE + INTERVAL 1 day;";
 
     // DON secrets slot ID (assuming 0)
     uint8 constant SLOT_ID = 0;
